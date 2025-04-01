9fa7cc8d5d66ce8d55a8f782b69154729658ec58
wandom-boi <hqpham196@gmail.com>
2025-04-01 23:15:03 +1100

    page.tsx first update

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,13 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/the-strata-portal.png"
+          alt="Logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <p>The internet portal related to management of the Strata Estate.</p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
