562d3bd163db1beb5a38b67bb463d893b5c6663f
wandom-boi <hqpham196@gmail.com>
2025-04-01 23:19:14 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/the-strata-portal.png"
           alt="Logo"
-          width={180}
-          height={38}
+          width={869}
+          height={144}
           priority
         />
         <p>The internet portal related to management of the Strata Estate.</p>
@@ -22,9 +21,10 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
+              className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={200}
+              width={20}
               height={20}
             />
             Deploy now
