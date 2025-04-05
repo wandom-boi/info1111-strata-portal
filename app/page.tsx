5e07d179ba1ad061c3cebf6e799dc92f06515af1
Quang Hung <qpha0812@uni.sydney.edu.au>
2025-04-05 17:27:34 +1100

    Added login screen (underdeveloped)

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          src="/the-strata-portal.png"
+          alt="Logo"
+          width={869}
+          height={144}
+          priority
+        />
+        <p>The internet portal related to management of the Strata Estate.</p>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[200px]"
+            href="login.tsx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Login to portal
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[200px]"
+            href="/strata-estate-about.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            About Strata Estate
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
