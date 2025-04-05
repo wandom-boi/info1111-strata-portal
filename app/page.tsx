2561f66fee83f8742575496577b1fa1015b1f428
Quang Hung <qpha0812@uni.sydney.edu.au>
2025-04-05 18:33:39 +1100

    praying

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[200px]"
-            href="./login.tsx"
+            href="/login"
             target="_blank"
             rel="noopener noreferrer"
           >
